ceab7486405c69ca6871f7e3e4e4d56c20672333
Albert Maristany <maristany.ramos.albert@gmail.com>
2016-05-20 21:02:57 +0200

    Test de checkin

--- src/main/java/Twitendencias.java
+++ src/main/java/Twitendencias.java
@@ -26,6 +26,7 @@ public class Twitendencias {
 	public static void main(String[] args) throws Exception {
 		System.setProperty("hadoop.home.dir", HADOOP_COMMON_PATH);
 		// Test de get
+		// Test de check in
 		SparkConf conf = new SparkConf().setAppName("Twitendencias").setMaster("local[*]");
 		JavaSparkContext ctx = new JavaSparkContext(conf);
 		JavaStreamingContext jsc = new JavaStreamingContext(ctx, new Duration(1000));
